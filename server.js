1c1c5cfc5bf8d2063d50a0b13e406679d141c76f
Charles Clemens <cclemensjr@gmail.com>
2018-10-23 13:29:02 -0700

    Started feature 2

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 'use strict';
 
-
-
-const express = require('express');
-
-const cors = require('cors');
 require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
 
-app.get('/location',(req ,res) => {
-  const locationInfo = convertToLatLong(req.query.data);
-  res.send(locationInfo);
-})
+function Location(data){
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
+  this.temperature = data.currently.temperature;
+  this.weatherSummary = data.currently.summary;
+}
 
 function convertToLatLong(data){
   const geoData = require('./data/geo.json');
@@ -23,28 +23,26 @@ function convertToLatLong(data){
   location.search_query = data;
   return location;
 }
-function Location(data){
-  this.formatted_query = data.formatted_address;
-  this.latitude = data.geometry.location.lat;
-  this.longitude = data.geometry.location.lng;
 
+function weatherInfo(data) {
+  const weatherData = require('./data/darksky.json');
+  //currently.summary & currently.temperature
+  const location = new Location(weatherData.results);
+  location.search_query = data;
+  return location;
 
 }
 
-// function searchToLatLong(query) {
-//   // Go to google (tomorrow)
-//   const geoData = require('./data/geo.json');
-//   const location = new Location(geoData.results[0]);
-// 	location.search_query = query;
-//   return location;
-// }
 
-// function Location(data) {
-//   this.formatted_query = data.formatted_address;
-//   this.latitude = data.geometry.location.lat;
-//   this.longitude = data.geometry.location.lng;
-// }
+app.get('/location',(req ,res) => {
+  const locationInfo = convertToLatLong(req.query.data);
+  res.send(locationInfo);
+})
 
+app.get('/weather', (req, res) =>{
+  const forecast = weatherInfo(req.query.data);
+  res.send(forecast);
+})
 
 
 app.listen(PORT, () => console.log(`App is up on ${PORT}`) );
